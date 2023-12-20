c54c5d4dae38ee615162afdecd52ddb143a91fff
Karen Lyon <144794853+Karelyon@users.noreply.github.com>
2023-12-20 13:13:16 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,40 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { applyStyles } from "@popperjs/core";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = function() {};
+let logos = ["♦", "♥", "♠", "♣"];
+let randomlogos = Math.floor(Math.random() * logos.length);
+let showLogo = logos[randomlogos];
+
+let pinta = document.querySelectorAll(".logo");
+pinta[0].innerHTML = showLogo;
+pinta[1].innerHTML = showLogo;
+
+let showred = document.querySelectorAll(".logo");
+if (showLogo == "♦" || showLogo == "♥") {
+  (showred[0].style.color = "red") && (showred[1].style.color = "red");
+} else {
+  (showred[0].style.color = "black") && (showred[0].style.color = "black");
+}
+
+let numbers = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+
+let randomNumbers = Math.floor(Math.random() * numbers.length);
+let shownumber = numbers[randomNumbers];
+document.querySelector(".number").innerHTML = shownumber;
